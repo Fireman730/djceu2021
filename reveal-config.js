010343201b9a17812f238435e3c440d9a1fad75f
Florian Haas <xahteiwi@gmail.com>
2021-05-22 20:54:14 +0200

    Update index.html and reveal-config.js configuration
    Fix references in index.html, and configuration in reveal-config.js,
    for reveal.js 4.
    
    Reference: https://revealjs.com/upgrading/

--- reveal-config.js
+++ reveal-config.js
@@ -2,6 +2,14 @@
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
 
+    plugins: [
+	RevealMarkdown,
+	RevealHighlight,
+	RevealNotes,
+	RevealZoom,
+	RevealMenu
+    ],
+
     controls: false,
 
     progress: true,
@@ -18,29 +26,11 @@ Reveal.initialize({
 
     menu: {
         themes: true,
-        themesPath: 'reveal.js/css/theme',
+        themesPath: 'reveal.js/dist/theme',
         transitions: false,
         openButton: false,
         openSlideNumber: true,
         markers: true
     },
 
-
-
-
-
-    // Optional libraries used to extend on reveal.js
-    dependencies: [
-        { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-        { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-
-
-        { src: 'reveal.js-menu/menu.js', async: true, condition: function() { return !!document.body.classList; } },
-
-
-    ]
 });
